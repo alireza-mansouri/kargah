048fda9fbbdcad5e480f11c9197b35007548fd96
alireza.mznsoory <alireza.mznsoory.1384@gmail.com>
2023-12-05 15:49:09 +0330

    calculator.c

--- calculator.c
+++ calculator.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+int pow(int a) {
+	return a * a ;
+}
 int calculate(int a, int b) {
 // Some calculation
 return a * b * 2 -10 +(a + b);
--- greeting.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-	printf("hello , world!\n") ;
-	printf("%d",402106586);
-	return 0 ;
-}
